9cb9e113973b02d118060c458168e6b22453074e
Mrunal2525 <MRUNAL.SURESH-SHINDE@CAPGEMINI.COM>
2025-02-12 12:51:45 +0530

    Update Program.cs

--- helloqet/Program.cs
+++ helloqet/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World mrunal!");
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello mrunal");
         }
     }
 }
